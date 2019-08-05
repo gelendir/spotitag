0ba97161f5112b78acf5b27bfb03ca941cc2019d
Gregory Eric Sanderson <gregory.eric.sanderson@gmail.com>
2019-08-05 19:02:21 -0400

    improve playlist import

--- spotitag/metadata.py
+++ spotitag/metadata.py
@@ -1,34 +1,46 @@
-
 class Metadata:
 
     def __init__(self, sp):
         self.sp = sp
 
-    def find_playlist(self, name):
+    def find_user_playlists(self, name):
+        return list(self.iter_user_playlists(name))
+
+    def iter_user_playlists(self, name):
         offset = 0
         response = self.sp.current_user_playlists()
         while len(response['items']) != 0:
             for playlist in response['items']:
-                if playlist['name'] == name:
-                    return playlist
+                if name.lower() in playlist['name'].lower():
+                    yield playlist
             offset += 50
             response = self.sp.current_user_playlists(offset=offset)
+
+    def search_playlists(self, term, limit=10):
+        response = self.sp.search(term, type='playlist', limit=limit)
+        return response['playlists']['items']
 
     def tracks(self, playlist_id):
         return list(self.iter_tracks(playlist_id))
 
     def iter_tracks(self, playlist_id):
         user = self.sp.current_user()
-        playlist = self.sp.user_playlist(user['id'], playlist_id)
-        tracks = playlist['tracks']
+        response = self.sp.user_playlist(user['id'], playlist_id)
+        tracks = self.valid_tracks(response['tracks']['items'])
 
         yield from self.extract_metadata(tracks)
-        while tracks['next']:
-            tracks = self.sp.next(tracks)
+        while response['tracks']['next']:
+            response = self.sp.next(tracks)
+            tracks = self.valid_tracks(response['tracks']['items'])
             yield from self.extract_metadata(tracks)
 
+    def valid_tracks(self, tracks):
+        for track in tracks:
+            if track['track'].get('id'):
+                yield track
+
     def extract_metadata(self, tracks):
-        for track in tracks['items']:
+        for track in tracks:
             bpm = self.find_bpm(track['track'])
             yield {'id': track['track']['id'],
                    'name': track['track']['name'],
